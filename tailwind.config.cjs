b4bec4eb2d55621c5b5bae23aba94b666cf09e82
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2022-10-22 09:38:41 +0300

    updated: most

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -108,5 +108,6 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp'),],
+  plugins: [require('@tailwindcss/line-clamp'),
+  require('@tailwindcss/forms'),],
 }
